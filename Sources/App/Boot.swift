9994e433ff1b64d3a4f936061e6ecc16b8bdb446
Rob Rickard of CommonWorld <commonworld@commonworldinc.com>
2018-05-25 15:01:19 +0200

    Cleaned up - running clean with no custom game code. (only hello world) ==> http://localhost:8080/hello

---
+++ Sources/App/Boot.swift
@@ -0,0 +1,17 @@
+//
+//  new.swift
+//  GameServer
+//
+//  Created by Rob Rickard on 5/25/18.
+//
+
+
+import Routing
+import Vapor
+
+/// Called after your application has initialized.
+///
+/// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#bootswift)
+public func boot(_ app: Application) throws {
+    // your code here
+}
---
+++ Sources/App/new.swift
@@ -0,0 +1,7 @@
+//
+//  new.swift
+//  GameServer
+//
+//  Created by Rob Rickard on 5/25/18.
+//
+
